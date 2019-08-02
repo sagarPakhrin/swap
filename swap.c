d22338373b6615aca33f323830c4ecc97bb7338b
sagar <sagarlama826@gmail.com>
2019-08-02 10:58:57 +0545

    values swapped

--- swap.c
+++ swap.c
@@ -2,8 +2,15 @@
 
 int main(){
 		int x = 10;
-		int y = 20;
+		int y=20;
 
 		printf("Before Swapping\n");
 		printf("X : %d\nY: %d\n",x,y);
+
+		int temp;
+		temp = x;
+		y = x;
+		x = temp;
+
+
 }
