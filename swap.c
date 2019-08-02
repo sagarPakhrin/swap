a36ba1fa5d9d37fd6750510a37158fa4c0465cfc
root <root@localhost.localdomain>
2019-08-02 11:44:23 +0545

    Value Changed To 40

--- swap.c
+++ swap.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 int main(){
-		int x = 10;
+		int x = 40;
 		int y = 20;
-			
 		printf("Before Swapping\n");
 		printf("X : %d\nY: %d\n",x,y);
-
 		int temp;
 		temp=x;
 		y=x;
