d5a331954d5e0a05a736ff1a5a15825a830d6576
kunjabijukchhe <bijukchhekunja@gamil.com>
2019-08-02 11:13:54 +0545

    values printed

--- swap.c
+++ swap.c
@@ -7,9 +7,14 @@ int main(){
 		printf("Before Swapping\n");
 		printf("X : %d\nY: %d\n",x,y);
 
-		
+
 		int temp;
 		temp=x;
 		y=x;
 		x=temp;
+
+		printf("After Swapping\n");
+		printf("x:%d\ny:%d\n",x,y);
+
+
 }
